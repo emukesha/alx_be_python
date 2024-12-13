8c62f90db0b473d060ad3376f3eefcf1322ad96b
rwandashi <mkneugene1@gmail.com>
2024-12-13 20:33:11 +0200

    First command

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+# Addition of 10 and 5 is 15
+# Subtraction of 10 and 5 is 5
+# Multiplication of 10 and 5 is 50
+
+print(f"Addition of {number1} and {number2} is {number1+number2}")
+print(f"Subtraction of {number1} and {number2} is {number1-number2}")
+print(f"Multiplication of {number1} and {number2} is {number1*number2}")
