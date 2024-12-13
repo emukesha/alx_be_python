6ea8f340d0b4bd29d2cc2750940288fd508f9a49
rwandashi <mkneugene1@gmail.com>
2024-12-13 21:17:02 +0200

    Removed the error

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = input("Enter your monthly income:")
 monthly_expenses = input("Enter your total monthly expenses:")
 
-monthly_savings = float(monthly_income) - float(monthly_income)
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 
 projected_savings = (monthly_savings  * 12) + (monthly_savings * 12 * 0.05)
 
