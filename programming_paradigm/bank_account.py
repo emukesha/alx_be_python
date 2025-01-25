e6fd64df01e6df3834ebdbf8d6a010bb44dacda6
rwandashi <mkneugene1@gmail.com>
2025-01-25 14:26:37 +0200

    Classes

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
+    def deposit(self,amount):
+        self.account_balance = self.account_balance + amount
+        return f"Deposited: ${self.account_balance}"
+    
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance = self.account_balance - amount
+            return f"Withdrew: ${amount}"
+        return "Insufficient funds."
+    
+    def display_balance(self):
+        return f"Current Balance: ${self.account_balance}"
