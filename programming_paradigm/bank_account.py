5bb2734c2192b316d71ec2861eba07ee98332de4
rwandashi <mkneugene1@gmail.com>
2025-01-25 14:58:13 +0200

    Classes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,8 +8,10 @@ class BankAccount:
         return self.account_balance
     
     def withdraw(self, amount):
-        self.account_balance = self.account_balance - amount
-        return self.account_balance
+        if self.account_balance >=amount:
+            self.account_balance = self.account_balance - amount
+            return True
+        return False
             
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -15,8 +15,7 @@ def main():
         account.deposit(amount)
         print(f"Deposited: ${amount}")
     elif command == "withdraw" and amount is not None:
-        if account.account_balance >= amount:
-            account.withdraw(amount)
+        if account.withdraw(amount):
             print(f"Withdrew: ${amount}")
         else:
             print("Insufficient funds.")
